3b5d07f1e077d90e0e8401f077c62cd1eae7299a
Pontus Palmenäs <pontus.palmenas@seb.se>
2021-12-14 18:51:31 +0100

    Day10b

--- src/main/java/Day10.java
+++ src/main/java/Day10.java
@@ -1,14 +1,12 @@
 import util.FileUtil;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Stack;
+import java.util.*;
 
 public class Day10 {
     public static void main(String[] args) {
         var in = FileUtil.read("input/day10.txt");
         System.out.println(solve1(in));
-        //System.out.println(solve2(in));
+        System.out.println(solve2(in));
     }
 
     private static int solve1(List<String> l) {
@@ -25,6 +23,25 @@ public class Day10 {
         }
 
         return score;
+    }
+
+    private static long solve2(List<String> l) {
+        var incomplete = new ArrayList<String>();
+        l.forEach(s -> {if (parse(s).isEmpty()) incomplete.add(s);});
+
+        var scores = new ArrayList<Long>();
+        for (String s : incomplete) {
+            long score = 0;
+            var fix = complete(s);
+
+            for (char c : fix.toCharArray()) {
+                score *= 5;
+                score += " )]}>".indexOf(c);
+            }
+            scores.add(score);
+        }
+
+        return scores.stream().sorted().toList().get(scores.size()/2);
     }
 
     private static Optional<Character> parse(String s) {
@@ -45,12 +62,34 @@ public class Day10 {
         return Optional.empty();
     }
 
+    private static String complete(String s) {
+        var stack = new Stack<Character>();
+        var missing = "";
+
+        for (char c : s.toCharArray()) {
+            if (isOpen(c)) {
+                stack.push(c);
+                continue;
+            }
+            stack.pop();
+        }
+        while (!stack.empty()) {
+            missing += matching(stack.pop());
+        }
+
+        return missing;
+    }
+
     private static boolean isOpen(char c) {
         return c == '(' || c == '[' || c == '{' || c == '<';
     }
 
     private static char matching(char c) {
         return switch (c) {
+            case '(' -> ')';
+            case '[' -> ']';
+            case '{' -> '}';
+            case '<' -> '>';
             case ')' -> '(';
             case ']' -> '[';
             case '}' -> '{';

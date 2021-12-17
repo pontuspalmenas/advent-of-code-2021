cae722fb9c9e0bdedec24ce597b7c7b9ff07f1fe
= <=>
2021-12-17 15:41:22 +0100

    wip

--- src/main/java/Day11.java
+++ src/main/java/Day11.java
@@ -2,8 +2,11 @@ import util.FileUtil;
 import util.Position;
 import util.Util;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Day11 {
     public static void main(String[] args) {
@@ -14,17 +17,39 @@ public class Day11 {
 
     private static int solve1(int[][] grid) {
         int flashes = 0;
-        debug(grid);
 
-        for (int step=0;step<1;step++) {
+        for (int step = 0; step < 2; step++) {
+            System.out.println("after step: " + step);
+            debug(grid);
+
+            for (int y = 0; y < grid.length; y++) {
+                for (int x = 0; x < grid[0].length; x++) {
+                    grid[y][x]++;
+                }
+            }
+
             var willFlash = new HashSet<>(shouldFlash(grid));
-            
 
             while (!willFlash.isEmpty()) {
+                var it = willFlash.iterator();
+                var addLater = new ArrayList<Position>();
+                while (it.hasNext()) {
+                    var p = it.next();
 
+                    for (var np : Util.neighbors(p.y(), p.x(), grid)) {
+                        if (!willFlash.contains(np)) {
+                            grid[np.y()][np.x()]++;
+                            if (grid[np.y()][np.x()] > 9) {
+                                addLater.add(np);
+                            }
+                        }
+                    }
+                    grid[p.y()][p.x()] = 0;
+                    it.remove();
+                }
+                willFlash.addAll(addLater);
             }
         }
-
         return flashes;
     }
 

159a1fd3734654e7bf4ee48a4a11482d92869a3b
Pontus Palmenäs <pontus@palmenas.se>
2022-01-04 18:13:38 +0100

    Day8

--- src/main/java/Day08.java
+++ src/main/java/Day08.java
@@ -19,91 +19,92 @@ public class Day08 {
     }
 
     private static long solve2(List<String> l) {
+        int result = 0;
         for (String s : l) {
-            var inputs = Set.copyOf(Arrays.stream(s.split("\\|")[0].split(" ")).toList());
-            var outputs = s.split("\\|")[1];
+            // pre-sort the input, need to map all len6 before len5
+            var inputs = Arrays.stream(sortByLen(s.split("\\|")[0])).map(Day08::chars).toList();
+            var outputs = Arrays.stream(s.split("\\| ")[1].split(" ")).map(Day08::chars).toList();
 
             var keys = keys(inputs);
-            var x = 0;
+
+            result += 1000 * keys.get((outputs.get(0))) +
+                    100 * keys.get(outputs.get(1)) +
+                    10 * keys.get(outputs.get(2)) +
+                    keys.get(outputs.get(3));
         }
 
-        throw new RuntimeException("Not Yet Implemented");
+        return result;
     }
 
-    /*
-        0 -> 6
-        1 -> 2
-        2 -> 5
-        3 -> 5
-        4 -> 4
-        5 -> 5
-        6 -> 6
-        7 -> 3
-        8 -> 7
-        9 -> 6
-     */
+    private static String[] sortByLen(String s) {
+        String[] ss = s.split(" ");
+        Arrays.sort(ss, (a, b)->Integer.compare(b.length(), a.length()));
+        return ss;
+    }
 
-    private static Map<String, Integer> keys(Set<String> inputs) {
-        // 1 -> 2
-        // 4 -> 4
-        // 7 -> 3
-        // 8 -> 7
+
+    private static Map<Set<Character>, Integer> keys(List<Set<Character>> inputs) {
         var knownLengths = Map.of(2,1,4,4,3,7,7,8);
 
-        var keys = new HashMap<String, Integer>();
-        var reverse = new HashMap<Integer, String>();
-        var unmapped = new HashSet<>(inputs);
+        var mapper = new Mapper();
 
         // First map all keys of known length
         for (int len : knownLengths.keySet()) {
-            int val = knownLengths.get(len);
-            String key = unmapped.stream().filter(s -> s.length() == len).findFirst().orElseThrow();
-            keys.put(key, val);
-            reverse.put(val, key);
-            unmapped.remove(key);
+            var key = inputs.stream().filter(p -> p.size() == len).findFirst().orElseThrow();
+            mapper.add(key, knownLengths.get(len));
         }
 
         // Deduce the rest
-        while (true) {
-            if (unmapped.isEmpty()) break;
-        }
-
-        return keys;
-    }
-
-    private static String deduceNext(Set<String> unmapped, Map<Integer, String> reverse) {
-        return "";
-    }
-
-    private static boolean containsAnyPermutation(String s) {
-        for (String p : permute(s)) {
-            if (s.contains(p)) return true;
-        }
-
-        return false;
-    }
-
-    public static Set<String> permute(String str) {
-        Set<String> set = new HashSet<String>();
-
-        // check if string is null
-        if (str == null) {
-            return null;
-        } else if (str.length() == 0) {
-            set.add("");
-            return set;
-        }
-
-        char first = str.charAt(0);
-        String sub = str.substring(1);
-
-        Set<String> words = permute(sub);
-
-        for (String strNew : words) {
-            for (int i = 0;i<=strNew.length();i++){
-                set.add(strNew.substring(0, i) + first + strNew.substring(i));
+        for (var set : inputs) {
+            if (mapper.mapped(set)) continue;
+            int len = set.size();
+            if (len == 6) { // candidates 6,9,0
+                if (!contains(set,1,mapper)) mapper.add(set,6);
+                else if (contains(set,4,mapper)) mapper.add(set,9);
+                else mapper.add(set,0);
+            }
+            if (len == 5 && !mapper.mapped(set)) { // candidates 2,3,5
+                if (containedIn(set,6,mapper)) mapper.add(set, 5);
+                else if (containedIn(set,9,mapper) && contains(set,1,mapper)) mapper.add(set, 3);
+                else mapper.add(set, 2);
             }
         }
+
+        return mapper.keys();
+    }
+
+    private static Set<Character> chars(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
         return set;
+    }
+
+    private static boolean contains(Set<Character> set, int n, Mapper m) {
+        return set.containsAll(m.get(n));
+    }
+
+    private static boolean containedIn(Set<Character> set, int n, Mapper m) {
+        if (m.get(n)==null) return false;
+        return m.get(n).containsAll(set);
+    }
+
+    static class Mapper {
+        private final HashMap<Set<Character>, Integer> keys = new HashMap<>();
+        private final HashMap<Integer, Set<Character>> rev = new HashMap<>();
+        void add(Set<Character> s, int val) {
+            keys.put(s, val);
+            rev.put(val, s);
+        }
+        boolean mapped(Set<Character> s) {
+            return keys.containsKey(s);
+        }
+        Set<Character> get(int n) {
+            return rev.get(n);
+        }
+        Map<Set<Character>, Integer> keys() {
+            return keys;
+        }
     }
 }
